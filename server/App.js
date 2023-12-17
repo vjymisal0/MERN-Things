aef17d20abd4733b72a2b693b62c508a8b8ab770
Vijay Misal <misalvijay153@gmail.com>
2023-12-17 13:11:50 +0530

    index

---
+++ server/App.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+
+const middleware = (req, res, next) => {
+    console.log("Hello my middleware")
+    next()
+}
+// middleware()
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.get("/about", middleware, (req, res) => {
+    res.send("Hello World About")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Contact form")
+})
+
+app.get("/signin", (req, res) => {
+    res.send("Login form")
+})
+
+app.get("/signup", (req, res) => {
+    res.send("Signup form")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
