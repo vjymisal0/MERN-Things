edc646cf8a6771b3771f56b4e68ef6f78583f5b2
Vijay Misal <misalvijay153@gmail.com>
2023-12-18 23:03:39 +0530

    shorten app.js

--- App.js
+++ App.js
@@ -8,40 +8,13 @@ const User = require("./model/userSchema")
 dotenv.config({ path: './config.env' })
 
 app.use(require('./routes/auth'))
-
 require("./db/conn")
+
 
 app.use(express.json())
 
 const PORT = process.env.PORT
 
-
-//middleware
-const middleware = (req, res, next) => {
-    console.log("Hello my middleware")
-    next()
-}
-
-
-// app.get('/', (req, res) => {
-//     res.send("Hello World")
-// })
-
-app.get("/about", middleware, (req, res) => {
-    res.send("Hello World About")
-})
-
-app.get("/contact", (req, res) => {
-    res.send("Contact form")
-})
-
-app.get("/signin", (req, res) => {
-    res.send("Login form")
-})
-
-app.get("/signup", (req, res) => {
-    res.send("Signup form")
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}: click here-> http://localhost:3000`)
