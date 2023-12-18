a9eb4210705e3da1bdc8a06d771d9b5a66309353
Vijay Misal <misalvijay153@gmail.com>
2023-12-18 22:54:26 +0530

    ms

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
+    name1: {
         type: String,
         required: true
     },
--- routes/auth.js
+++ routes/auth.js
@@ -13,11 +13,23 @@ router.get('/', (req, res) => {
 router.post('/register', (req, res) => {
     // console.log(req.body)
     // res.json({ message: req.body })
-    const { name, email, phone, work, password, cpassword } = req.body
+    const { name1, email, phone, work, password, cpassword } = req.body
 
-    if (!name || !email || !phone || !work || !password || !cpassword) {
+    if (!name1 || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({ error: "Please fill all the fields" })
     }
+    User.findOne({ email: email })
+        .then((userExist) => {
+            if (userExists) {
+                return res.status(422).json({ error: "Email already exists" })
+            }
+            const user = new User({ name1, email, phone, work, password, cpassword })
+            user.save().then(() => {
+                res.status(201).json({ message: "User registered successfully" })
+            }).catch((err) => res.status(500).json({ error: "Failed to register" }))
+        })
+        .catch(err => { console.log(err) })
+
 })
 
 module.exports = router;
