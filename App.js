562a9db9fe01aade1eb349b33891c376c7776b56
Vijay Misal <misalvijay153@gmail.com>
2023-12-18 11:18:27 +0530

    conn added

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ dotenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE
 
-
 mongoose.connect(DB).then(() => {
     console.log("Connection successful")
 }).catch((err) => console.log("No connection"))
