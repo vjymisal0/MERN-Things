a050ad374f5adbdc2401db2049de539469357d2c
Vijay Misal <misalvijay153@gmail.com>
2023-12-22 22:23:12 +0530

    CRUD

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
+const fs = require('fs')
+const index = fs.readFileSync('index.html', 'utf-8')
 const express = require('express');
+const data = fs.readFileSync('Vijay.json', 'utf-8')
 const morgan = require('morgan');
 const server = express();
+// server.use(morgan('default'))//its deprecated
+server.use(morgan('dev'))
+server.use(express.static('public'))
+const products = data.products
+server.use(express.json())
+// server.get('/', (req, res) => {
+//     res.send(index);
+// })
+
+
 
 // server.get('/', (req, res) => {
 //     res.send("Hello world from the server");
@@ -32,25 +45,51 @@ const auth = (req, res, next) => {
 
 //API -ENDPOINTS
 
-server.get('/product/:id', auth, (req, res) => {        
-    console.log(req.params)
+// server.get('/product/:id', auth, (req, res) => {
+//     console.log(req.params)
+//     res.json({ type: 'GET' })
+// }
+// )
+// Read GET /products
+server.get('/products', (req, res) => {
     res.json({ type: 'GET' })
-}
-)
-server.post('/register', auth, (req, res) => {
-    res.json({ type: 'POST' })
-}
-)
+})
+// Read GET /products/:id
+server.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+    const product = products.find(p => p.id === id)
+    res.json(product)   // it will send the product in json format
+})
+//create POST /products
+server.post('/products', (req, res) => {
+    console.log(req.body)
+    products.push(req.body)
+    res.status(201).json();
+})
+//update PUT /products/:id
+server.put('/products/:id', (req, res) => {
+    const id = req.params.id;
+    const productIndex = products.findIndex(p => p.id === id)
+    products.splice(productIndex, 1, { ...req.body, id: id })
+    res.status(201).json();
+})
 
-server.put('/login', auth, (req, res) => {
-    res.json({ type: 'PUT' })
-}
-)
 
-server.delete('/', (req, res) => {
-    res.json({ type: 'DELETE' })
-}
-)
+
+// server.post('/register', auth, (req, res) => {
+//     res.json({ type: 'POST' })
+// }
+// )
+
+// server.put('/login', auth, (req, res) => {
+//     res.json({ type: 'PUT' })
+// }
+// )
+
+// server.delete('/', (req, res) => {
+//     res.json({ type: 'DELETE' })
+// }
+// )
 
 
 server.listen(3000, () => {
